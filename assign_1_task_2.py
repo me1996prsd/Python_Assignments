09c61ea130adeefa8cf10d7066017af5815db39a
me1996prsd <151220516+me1996prsd@users.noreply.github.com>
2025-04-28 18:00:06 +0530

    Add files via upload

---
+++ assign_1_task_2.py
@@ -0,0 +1,3 @@
+first = input("Enter First Name : ")
+second = input("Enter Second Name : ")
+print("Hello," + (first +" " + second) + "!" + "Welcome to the python program")
---
+++ assignment_1_task_1.py
@@ -0,0 +1,17 @@
+# Mathematical operations
+a = input("Enter first number")
+b = input("Enter first number")
+a = int(a)
+b = int(b)
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+c = int(c)
+d = int(d)
+e = int(e)
+f = float(f)
+print ("Addition : ",c)
+print ("Subtraction : ",d)
+print ("Multiplication : ",e)
+print ("Division : ",f)
