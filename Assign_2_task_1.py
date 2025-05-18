9c33627fb8f3ee0141a9ef975f263426f8babb0a
me1996prsd <151220516+me1996prsd@users.noreply.github.com>
2025-05-18 22:44:50 +0530

    Add files via upload

---
+++ Assign_2_task_1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter Any Number : "))
+if a%2==0:
+    print("The Number is Even")
+else :
+    print("The Number is Odd")
+    
---
+++ assign_2_task_2.py
@@ -0,0 +1,4 @@
+sum = 0
+for num in range(1,51):
+    sum +=num
+print (f"The Sum of Number from 1 to 50 is {sum}")
